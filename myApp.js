86b81411bd15de298cd4ed8bfdd7a0391553e6ff
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-16 21:57:57 +0100

    Add env variable to transform the message content returned by the json endpoint

--- myApp.js
+++ myApp.js
@@ -1,11 +1,21 @@
 var express = require('express');
+const res = require('express/lib/response');
 var app = express();
+require('dotenv').config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
-app.get("/json", (req, res) => res.json({message: "Hello json"}));
+app.get("/json", (req, res) => {
+    let message = "Hello json";
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    
+    res.json({message: message});
+});
 
 
 
