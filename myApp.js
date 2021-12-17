529cb6c03645f2e2c5ac89e00d2d7675791c46e1
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-17 18:01:43 +0100

    Handle POST request to return the complete name from the request body

--- myApp.js
+++ myApp.js
@@ -38,10 +38,15 @@ app.get("/:word/echo", (req, res) =>
     })
 );
 
-app.get("/name", (req, res) =>
-    res.json({
-        name: `${req.query.first} ${req.query.last}`
-    })
+app.route("/name")
+    .get((req, res) =>
+        res.json({
+            name: `${req.query.first} ${req.query.last}`
+        }))
+    .post((req, res) => 
+        res.json({ 
+            name: `${req.body.first} ${req.body.last}`
+        })
 );
 
  module.exports = app;
